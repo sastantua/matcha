9e60a99b49d256da25ec28b08cd3818b301f98b0
nicovrg <nicolasvergne88@gmail.com>
2020-01-05 23:26:00 +0100

    react icon spin feature

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import spin from './spin.js'
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<button className="App-btn">spin</button>
-				<p>Coucou Matthieu</p>
-				<p>🤔😡🤬🤯🤪😋😎🤫</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Viens finir camagru</a>
+				<img src={logo} className="App-logo" alt="logo"/>
+				<button className="App-btn" onClick={spin}>🤔</button>
 			</header>
 		</div>
 	);
