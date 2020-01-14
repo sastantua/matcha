ec5c09eea91f4fab29434e6d10824b40c31c8a21
Nicolas VERGNE <nicolasvergne88@gmail.com>
2020-01-15 00:40:27 +0100

    refacto clock component with hooks state and effect, now display time before blackhole

--- front/src/components/Clock/Clock.jsx
+++ front/src/components/Clock/Clock.jsx
@@ -1,45 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect,  } from 'react';
 
-function tick() {
-	setTime({time: new Date().toLocaleString()});
-}
-
-function Clock () {
+function Clock() {
+	const blackhole = new Date("1 May 2020").toLocaleString();
 	const [time, setTime] = useState(new Date().toLocaleString());
-	const [interval, setInterval] = useEffect(setInterval(tick(), 1000));
-		
-	// constructor(props) {
-	// 	super(props);
-	// 	this.state = {
-	// 		time: new Date().toLocaleString()
-	// 	};
-	// }
 
-	// componentDidMount() {
-	// 	this.intervalID = setInterval(
-	// 		() => this.tick(),
-	// 		1000
-	// 	);
-	// }
-		
-	componentWillUnmount() {
-		clearInterval(this.intervalID);
+	useEffect(() => {
+		const interval = setInterval(() => {setTime(new Date().toLocaleString())}, 1000)
+		return () => {
+			clearInterval(interval);
+		}
+	}, [])
+
+	let date = remaningTime(blackhole, time);
+
+	function remaningTime(blackhole, time) {
+		let bh = new Date(blackhole);
+		let today = new Date(time);
+		bh = bh.getTime() / 86400000;
+		today = today.getTime() / 86400000;
+		return new Number(bhtoday).toFixed(0).toLocaleString();
 	}
 
-	// tick() {
-	// 	this.setState({time: new Date().toLocaleString()});
-	// }
-
-	render() {
-		let date = this.state.time;
-		date = date.split(',');
-		return (
-			<p className="App-clock">
-				blackhole in : {date[1]} {date[0]}
-			</p>
-		);
-	}
-} 
+	return (<p>{date} days before blackhole</p>);
+}
 
 export default Clock;
 
@@ -47,14 +30,6 @@ export default Clock;
 /* 
 https://www.scriptol.fr/javascript/dates-difference.php
 
-var bh = new Date("30 April 2020");
-var today = new Date();
 
-function remaningTime(bh, today)
-{
-  bh = bh.getTime() / 86400000;
-  today = today.getTime() / 86400000;
-  return new Number(todaybh).toFixed(0);
-}
 
 */ 
--- front/src/components/Footer/Footer.jsx
+++ front/src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Footer } from 'react-bulma-components';
+// import { Footer } from 'react-bulma-components';
 
 import EmojiButton from '../EmojiButton/EmojiButton'
 import Clock from '../Clock/Clock';
