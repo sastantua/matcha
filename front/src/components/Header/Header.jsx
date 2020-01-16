e0a284c1dbe1208f2ae0adfbca8dba86991d359b
Nicolas VERGNE <nicolasvergne88@gmail.com>
2020-01-16 16:38:20 +0100

    test header with material ui classes

--- front/src/components/Header/Header.jsx
+++ front/src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { makeStyles } from '@material-ui/core/styles';
 import { AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import MailIcon from '@material-ui/icons/Mail';
@@ -8,38 +9,51 @@ import NotificationsIcon from '@material-ui/icons/Notifications';
 import './Header.css';
 // import { Navbar } from 'react-bulma-components';
 
+const useStyles = makeStyles(theme => ({
+	root: {
+		flexGrow: 1,
+	},
+	menuButton: {
+		marginRight: theme.spacing(2),
+	},
+	title: {
+		flexGrow: 1,
+	},
+}));
 
 function Header() {
+	const classes = useStyles();
 	return (
-		<AppBar position="static" id="header">
-			<Toolbar>
-				<IconButton edge="start" color="inherit" aria-label="menu">
-					<MenuIcon />
-				</IconButton>
-				<Button color="inherit">
-					<Link to="/" style={{ color: '#FFF' }}>
-						home
-					</Link>
-				</Button>
-				<Button color="inherit">
-					<Link to="/account" style={{ color: '#FFF' }}>
-						account
-					</Link>
-				</Button>
-				<Button color="inherit">
-					<Link to="/match" style={{ color: '#FFF' }}>
-						match
-					</Link>
-				</Button>
-				<IconButton edge="start" color="inherit" aria-label="menu">
-					<MailIcon />
-				</IconButton>
-				<IconButton edge="start" color="inherit" aria-label="menu">
-					<NotificationsIcon />
-				</IconButton>
-			</Toolbar>
-		</AppBar>
-
+		<div className={classes.root}>
+			<AppBar position="static" id="header">
+				<Toolbar>
+					<IconButton edge="start" color="inherit" aria-label="menu">
+						<MenuIcon />
+					</IconButton>
+					<Button className={classes.title} color="inherit">
+						<Link to="/" style={{ color: '#FFF' }}>
+							home
+						</Link>
+					</Button>
+					<Button className={classes.title} color="inherit">
+						<Link to="/account" style={{ color: '#FFF' }}>
+							account
+						</Link>
+					</Button>
+					<Button className={classes.title} color="inherit">
+						<Link to="/match" style={{ color: '#FFF' }}>
+							match
+						</Link>
+					</Button>
+					<IconButton edge="start" color="inherit" aria-label="menu">
+						<MailIcon />
+					</IconButton>
+					<IconButton edge="start" color="inherit" aria-label="menu">
+						<NotificationsIcon />
+					</IconButton>
+				</Toolbar>
+			</AppBar>
+		</div>
 
 		// <Navbar id="header">
 		// 	<Navbar.Brand>
