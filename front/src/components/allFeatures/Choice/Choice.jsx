f8609f84980edbbea6058fd05e2e5c0c4f9a1cab
nicovrg <nicolasvergne88@gmail.com>
2020-01-19 00:40:39 +0100

    FIX THIS FUCKING CLOCK THAT BROKE FOR NO REASON, typeof(var) => number, console.log(var) => NaN, fuck you Brendan Eich

--- front/src/components/allFeatures/Choice/Choice.jsx
+++ front/src/components/allFeatures/Choice/Choice.jsx
@@ -2,29 +2,28 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Button from '@material-ui/core/Button';
-import styled from "styled-components"
 
-import EmojiButton from '../../EmojiButton/EmojiButton'
+// import EmojiButton from '../../EmojiButton/EmojiButton'
 
 import './Choice.css'
 
 function Choice() {
 	/* React Logo Spin */
-	var duration = 5;
-	var changeDuration = () => {
-		let logo = document.getElementById("App-logo");
+	// var duration = 5;
+	// var changeDuration = () => {
+		// let logo = document.getElementById("App-logo");
 		// logo.style.width = "20em";
 		// logo.style.height = "20em";
-		duration > 0.15 ? duration -= .25 : duration = 1;
-		logo.animate([
-			{ transform: 'rotate(0deg)' },
-			{ transform: 'rotate(360deg)' }
-		],
-		{
-			duration: duration,
-			iterations: Infinity
-		})
-	}
+	// 	duration > 0.15 ? duration -= .25 : duration = 1;
+	// 	logo.animate([
+	// 		{ transform: 'rotate(0deg)' },
+	// 		{ transform: 'rotate(360deg)' }
+	// 	],
+	// 	{
+	// 		duration: duration,
+	// 		iterations: Infinity
+	// 	})
+	// }
 
 	return (
 		<div id="main-wrapper">
