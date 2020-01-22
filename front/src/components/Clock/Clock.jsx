4d29e6928637e5aab9ed4e9fbf4af5a833393331
Nicolas VERGNE <nicolasvergne88@gmail.com>
2020-01-22 03:03:02 +0100

    fix date that start bugging again

--- front/src/components/Clock/Clock.jsx
+++ front/src/components/Clock/Clock.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 
 function Clock() {
-	const blackhole = new Date("01/05/2020").toLocaleString();
+	const blackhole = new Date('May 5, 2020 03:24:00').toLocaleString();
+	// const blackhole = new Date("01/05/2020").toLocaleString();
 	const [time, setTime] = useState(new Date().toLocaleString());
 
 	useEffect(() => {
@@ -14,9 +15,10 @@ function Clock() {
 	let date = remaningTime(blackhole, time);
 
 	function remaningTime(blackhole, time) {
-		console.log(blackhole);
 		let bh = new Date(blackhole);
 		let today = new Date();
+		console.log({today});
+		console.log({blackhole});
 		return (Math.round((bhtoday) / (1000 * 60 * 60 * 24)).toFixed(0));
 	}
 	return (<p>{date} days before blackhole</p>);
