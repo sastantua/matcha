dfe1f6f5395dfdf8f024dd1566f0a761a8a4465b
nicovrg <nicolasvergne88@gmail.com>
2020-01-23 22:12:50 +0100

    change login and signup

--- front/src/api.js
+++ front/src/api.js
@@ -4,4 +4,9 @@ const api = axios.create({
 	baseURL: 'http://167.172.168.207:3000'
 });
 
+// username: neo4j
+// password: 42
+// http://167.172.168.207:7474
+// http://167.172.168.207:7474/browser/
+
 export default api;
