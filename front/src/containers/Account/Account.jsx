95d9df3f64fa987f0e7bdda320440321f9f82ba5
nicovrg <nicolasvergne88@gmail.com>
2020-02-06 17:57:33 +0100

    push before going to school

--- front/src/containers/Account/Account.jsx
+++ front/src/containers/Account/Account.jsx
@@ -1,12 +1,12 @@
 import React, {useContext} from 'react';
 
-import { UserContext } from '../../context/UserContext';
+import { UserContext } from '../../context/UserContext';
 
 function Account() {
 	const {userData, setUserData} = useContext(UserContext);
 
 	return (
-        <div>{userData}</div>
+        <div>{}</div>
 	);
 }
 
