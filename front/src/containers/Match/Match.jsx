145a9200e5e23233c19692686206a2c8d40d737f
nicovrg <nicolasvergne88@gmail.com>
2020-02-10 13:51:32 +0100

    little modfis

--- front/src/containers/Match/Match.jsx
+++ front/src/containers/Match/Match.jsx
@@ -5,7 +5,7 @@ import api from '../../api/api'
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 
-import { Button, Paper } from '@material-ui/core';
+import { Button, Paper, Typography, TextareaAutosize } from '@material-ui/core';
 import { Favorite as FavoriteIcon, ArrowForwardIos as ArrowForwardIosIcon, ArrowBackIos as ArrowBackIosIcon, AspectRatio as AspectRatioIcon } from '@material-ui/icons';
 import logo from '../../media/cerisier.jpg';
 import useStyles from '../../helper/useStyles'
@@ -21,13 +21,20 @@ function Match() {
 			<Header />
 			<Paper id="main-container" component='div'>
 				<img src={logo} id="profile-picture" alt="profile-picture"/>
+				<Typography id="username">Nico</Typography>
+				{/* <p id="bio">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+      				ut labore et dolore magna aliqua
+				</p> */}
+				<Typography id="bio">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+				    ut labore et dolore magna aliqua.
+				</Typography>
+
 				<div id="button-container">
 					<ArrowBackIosIcon htmlColor='#FAE3D9' />
 					<FavoriteIcon htmlColor='#FAE3D9' className="icon-btn" />
 					<ArrowForwardIosIcon htmlColor='#FAE3D9' className="icon-btn" />
-						{/* <Button color="secondary" type='submit'>like</Button> */}
-						{/* <Button color="secondary" type='submit'>skip</Button> */}
-					{/* </FavoriteIcon> */}
 				</div>
 			</Paper>
 			<Footer />
