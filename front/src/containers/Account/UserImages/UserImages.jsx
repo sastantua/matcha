0f494b53435ddc5e005808575dcc1b3f85f346f0
Nicolas VERGNE <nicolasvergne88@gmail.com>
2020-02-18 05:15:17 +0100

    start userImages and motif mobile colors and save button

--- front/src/containers/Account/UserImages/UserImages.jsx
+++ front/src/containers/Account/UserImages/UserImages.jsx
@@ -1,36 +1,112 @@
 import React from 'react';
+import { useState } from 'react';
 // import { useContext } from 'react';
-// import { UserContext } from '../../context/UserContext';
-// import api from '../../api/api'
+// import { UserContext } from '../../../context/UserContext';
+import api from '../../../api/api'
 
+import { makeStyles } from '@material-ui/core/styles';
+import { TextField,Button, styled as styledMaterial } from '@material-ui/core';
 import logo from '../../../media/cerisier.jpg';
 import './UserImages.css'
 
-function UserImages() {
-	// const classes = useStyles();
-	// const { user, setUser } = useContext(UserContext);
-	
-	// const handleSubmit = (e) => {
-	// 	e.preventDefault();
-	// 	api.post('/user/edit', user)
-	// 	.then((res) => {console.log(res);})
-	// 	.catch((err) => {console.log(err);})
-	// }
+const InputWrapper = styledMaterial(TextField)({
+	fontSize: '1rem',
+	width: '12rem',
+	color: '#OOB7FF'
+});
 
-	// const handleProfilePicture = (e) => {setUser({...user, email: e.target.value});}
+const useStyles = makeStyles({
+	root: {
+		background: '#FF3860',
+		border: 0,
+		borderRadius: 3,
+		boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+		color: 'white',
+		height: 48,
+		padding: '0 30px',
+		// marginTop: '10px',
+		// display: 'flex',
+		// justifyContent: 'center',
+		// alignItems: 'center',
+		// marginRight: '80px',
+	},
+});
+
+
+function UserImages() {
+	const classes = useStyles();
+	// const { user, setUser } = useContext(UserContext);
+	const [selectedFile, setSelectedFile] = useState({file: '', loaded: ''});
+	
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		// api.post('/user/edit', user)
+		// .then((res) => {console.log(res);})
+		// .catch((err) => {console.log(err);})
+	}
+
+	const addPictureFile = (e) => {
+		console.log("start");
+		console.log(e.target.files[0])
+		
+		setSelectedFile({
+			...selectedFile, 
+			file: e.target.files[0],
+			loaded: 0,
+		});	
+
+		api.post('/user/picture')
+		.then((res) => {
+			console.log(res);
+
+		})
+		.catch((err) => {
+			console.log(err);
+		})
+	}
+
+	console.log(selectedFile);
+
+	const uploadPicture = () => {
+		const data = new FormData() 
+		data.append('file', selectedFile.file)
+		api.post('/user/picture')
+		.then((res) => {
+			console.log(res);
+		})
+		.catch((err) => {
+			console.log(err);
+		})
+	}
 
 	return (
 		<div id="user-images-small">
-			<img src={logo} id="profile-picture" alt="profile" />
-			
-			
-			{/* <form id="edit-form" className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit}> */}
-				{/* <InputWrapper type="file" label={"profile-picture"} name="profile-picture" onChange={handleProfilePicture} variant="outlined"/> */}
-				{/* <input type="file" accept="image/*" id="imageDataFile" name="imageDataFile" style="display: none;"> */}
-				{/* <InputWrapper variant="outlined" label={ user && user.biography ? user.biography : "biography"} name="biography" onChange={handleBiography}/> */}
-			{/* </form> */}
+			<InputWrapper type="file" name="file" onChange={addPictureFile} variant="filled"/>
+			<Button type="button" className={classes.root} onClick={uploadPicture}>Upload</Button>
 		</div>
 	);
 }
 
 export default UserImages;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <img src={logo} id="profile-picture" alt="profile" /> */}
+{/* <form id="image-form" className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit}> */}
+	{/* <InputWrapper type="file" label={"profile-picture"} name="profile-picture" onChange={addPicture} variant="outlined"/> */}
+	{/* <input type="file" accept="image/*" id="imageDataFile" name="imageDataFile" style="display: none;"/> */}
+{/* </form> */}
