2ea3acab13741111a94588e4381a3d9283dd3313
nicovrg <nicolasvergne88@gmail.com>
2020-02-27 19:19:07 +0100

    start hobby in search

--- front/src/containers/Search/Popularity/Popularity.jsx
+++ front/src/containers/Search/Popularity/Popularity.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import { styled as styledMaterial } from '@material-ui/core';
 
 import { Typography } from '@material-ui/core';
-import PopularitySlider from '../SliderStyling/PopularityStyling';
+import PopularitySlider from './PopularityStyling';
 
 
 const PopularityContainer = styled.div`
