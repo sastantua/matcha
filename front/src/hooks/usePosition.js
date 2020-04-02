d8752d11f4b172544845f58f9d53e4b04b33828b
Guillaume Roux <guillaumeroux123@gmail.com>
2020-04-02 17:07:13 +0200

    eslint rules enhanced and fixes

--- front/src/hooks/usePosition.js
+++ front/src/hooks/usePosition.js
@@ -38,7 +38,7 @@ export default () => {
 				.catch(err => console.log(err))
 		}
 
-		const onError = (error) => {
+		const onError = () => {
 			console.log("locationFromIp from onError")
 			locationFromIp();
 			// if (error.code == error.PERMISSION_DENIED)
