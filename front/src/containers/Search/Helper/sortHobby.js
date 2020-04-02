7d483e87bc727026cb17154ac2496857bcc39533
Guillaume Roux <guillaumeroux123@gmail.com>
2020-04-02 16:29:24 +0200

    add airbnb eslint config

--- front/src/containers/Search/Helper/sortHobby.js
+++ front/src/containers/Search/Helper/sortHobby.js
@@ -2,18 +2,12 @@ function sortHobby(userHobbies, a, b) {
 	let resultA = 0;
 		let resultB = 0;
 		if (userHobbies && userHobbies.length) {
-			userHobbies.map(userHobby => {
-				a.hobbies.map(compareHobby => {
-					if (userHobby.name === compareHobby.name)
-						resultA++;
-				})
-			})
-			userHobbies.map(userHobby => {
-				b.hobbies.map(compareHobby => {
-					if (userHobby.name === compareHobby.name)
-						resultB++;
-				})
-			})
+			userHobbies.map(userHobby => 
+				a.hobbies.map(compareHobby => (userHobby.name === compareHobby.name) && resultA++)
+			)
+			userHobbies.map(userHobby => 
+				b.hobbies.map(compareHobby => (userHobby.name === compareHobby.name) && resultB++)
+			)
 
 		}
 		return (resultAresultB);
