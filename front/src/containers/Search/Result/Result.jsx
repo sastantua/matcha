ca3a0274f0daf1a27e9b9286409df62b01870f66
nicovrg <nicolasvergne88@gmail.com>
2020-04-24 03:31:50 +0200

    finish responsive style for search component

--- front/src/containers/Search/Result/Result.jsx
+++ front/src/containers/Search/Result/Result.jsx
@@ -26,61 +26,67 @@ const UserContainer = styled.div`
 	padding: ${SPACING.BASE};
 	background-image: linear-gradient(90deg, ${COLORS.ORANGE_GRADIENT} 30%, ${COLORS.PINK_GRADIENT} 90%);
 	box-shadow: 0px 0px 31px -5px rgba(0,0,0,0.75);
-	@media only screen and (max-width: ${BREAK_POINTS.SCREEN_SM}) {
+	@media only screen and (min-width: ${BREAK_POINTS.SCREEN_SM}) {
 		display: flex;
 		flex-direction: column;
-		/* justify-content: center; */
+	}
+	@media only screen and (max-width: ${BREAK_POINTS.SCREEN_SM}) {
 		padding: ${SPACING.XS};
 	}
 `
 
 const HeadContainer = styled.div`
 	display: flex;
-	@media only screen and (max-width: ${BREAK_POINTS.SCREEN_SM}) {
+	@media only screen and (min-width: ${BREAK_POINTS.SCREEN_SM}) {
 		flex-direction: row;
+	}
+	@media only screen and (max-width: ${BREAK_POINTS.SCREEN_SM}) {
+		flex-direction: column;
 		align-items: center;
 	}
 `
 
 const ImagesContainer = styled.div`
-	width: 40%;
 	@media only screen and (max-width: ${BREAK_POINTS.SCREEN_SM}) {
-		width: auto;
+		width: 50%;
+	}
+	@media only screen and (max-width: ${BREAK_POINTS.SCREEN_SM}) {
+		width: 100%;
 	}
 `
 
 const Infos = styled.div`
 	display: flex;
-	flex-direction: row;
 	flex-wrap: wrap;
 	width: 100%;
+	@media only screen and (min-width: ${BREAK_POINTS.SCREEN_SM}) {
+		flex-direction: row;
+	}
 	@media only screen and (max-width: ${BREAK_POINTS.SCREEN_SM}) {
 		flex-direction: column;
-		align-items: start;
-		justify-content: center;
-	}
-	@media only screen and (min-width: ${BREAK_POINTS.SCREEN_SM}) {
-		margin-left: ${SPACING.BASE};
+		margin-top: ${SPACING.XS}
 	}
 `
 
 const NameContainer = styled.div`
 	display: flex;
 	flex-direction: column;
-	width: 50%;
+	justify-content: center;
+	margin-left: ${SPACING.BASE};
 	@media only screen and (max-width: ${BREAK_POINTS.SCREEN_SM}) {
 		width: auto;
-		margin-left: ${SPACING.BASE}
+		margin-left: ${SPACING.XS};
 	}
 `
 
 const GenderOrientationAgeContainer = styled.div`
 	display: flex;
 	flex-direction: row;
-	width: 50%;
+	& > :nth-child(n+2) {
+		margin-left: ${SPACING.XXS}
+	}
 	@media only screen and (max-width: ${BREAK_POINTS.SCREEN_SM}) {
 		width: auto;
-		margin-left: ${SPACING.BASE}
 	}
 `
 
@@ -117,6 +123,10 @@ const Icon = styled.div`
 	display: flex;
 	justify-content: center;
 	align-items: center;
+	border-radius: 50%;
+	background-color: ${COLORS.PINK_FLASHY};
+	opacity: .7;
+	box-shadow: 0px 0px 102px -20px rgba(0,0,0,0.75);
 	@media only screen and (max-width: ${BREAK_POINTS.SCREEN_SM}) {
 		height: 12px;
 		width: 12px;
@@ -127,10 +137,6 @@ const Icon = styled.div`
 		width: 28px;
 		margin-right: ${SPACING.XS};
 	}
-	border-radius: 50%;
-	background-color: ${COLORS.PINK_FLASHY};
-	opacity: .7;
-	box-shadow: 0px 0px 102px -20px rgba(0,0,0,0.75);
 `
 
 const Chip = styled.div`
@@ -146,22 +152,23 @@ const Chip = styled.div`
 `
 
 const ChipsContainer = styled.div`
-	@media only screen and (max-width: ${BREAK_POINTS.SCREEN_SM}) {
-		flex-direction: row;
-	}
 	display: flex;
 	flex-wrap: wrap;
 	margin: ${SPACING.BASE} 0;
+	@media only screen and (max-width: ${BREAK_POINTS.SCREEN_SM}) {
+		flex-direction: row;
+		margin: ${SPACING.XS} 0;
+	}
 `
 
 const Bottom = styled.div`
+	margin-top: ${SPACING.BASE};
+	display: flex;
+	justify-content: space-evenly;
 	@media only screen and (max-width: ${BREAK_POINTS.SCREEN_SM}) {
 		flex-direction: column;
 		align-items: center;
 	}
-	margin-top: ${SPACING.BASE};
-	display: flex;
-	justify-content: space-evenly;
 `
 const Box = styled.div`
 	padding: ${SPACING.XXS} ${SPACING.XS};
@@ -170,41 +177,6 @@ const Box = styled.div`
 `
 
 function User(props) {
-	const { user } = useContext(UserContext);
-	const [like, setLike] = useState(props.user.liked);
-	const [block, setBlock] = useState(false);
-
-	const likeMatch = () => {
-		api.post(`/user/like/${props.user._id}`)
-		.then((res) => {setLike(true)})
-		.catch((err) => {console.log(err)})
-		notifSocket.emit('notification', {
-			type: 'like',
-			to: props.user._id,
-			from: user._id
-		})
-	}
-
-	const unlikeMatch = () => {
-		api.post(`/user/unlike/${props.user._id}`)
-		.then((res) => {setLike(false)})
-		.catch((err) => {console.log(err)})
-	}
-
-	const blockMatch = () => {
-		api.post(`/user/block/${props.user._id}`)
-		.then((res) => {
-			document.getElementById(props.user.username).style.display = "none";
-			setBlock(true)})
-		.catch((err) => {console.log(err)})
-	}
-
-	const unblockMatch = () => {
-		api.post(`/user/unblock/${props.user._id}`)
-		.then((res) => {setBlock(false)})
-		.catch((err) => {console.log(err)})
-	}
-
 	let hobbiesArray = [];
 	
 	if (props.user.hobbies.length <= 5)
@@ -230,12 +202,12 @@ function User(props) {
 						<NameContainer id="NameContainer">
 							<Text>{`@${props.user.username.charAt(0).toUpperCase() + props.user.username.slice(1)}`}</Text>
 							<Text>{`${props.user.firstname.charAt(0).toUpperCase() + props.user.firstname.slice(1)} ${props.user.lastname.charAt(0).toUpperCase() + props.user.lastname.slice(1)}`}</Text>
+							<GenderOrientationAgeContainer id="GenderOrientationAgeContainer">
+								<Text>{props.user.gender.name.charAt(0).toUpperCase() + props.user.gender.name.slice(1)}</Text>
+								<Text>{props.user.orientation.name.charAt(0).toUpperCase() + props.user.orientation.name.slice(1)}</Text>
+								<Text>{`${findAge(props.user.birthdate)} Yo`}</Text>
+							</GenderOrientationAgeContainer>
 						</NameContainer>
-						<GenderOrientationAgeContainer id="GenderOrientationAgeContainer">
-							<Text>{props.user.gender.name.charAt(0).toUpperCase() + props.user.gender.name.slice(1)}</Text>
-							<Text>{props.user.orientation.name.charAt(0).toUpperCase() + props.user.orientation.name.slice(1)}</Text>
-							<Text>{`${findAge(props.user.birthdate)} Yo`}</Text>
-						</GenderOrientationAgeContainer>
 					</Infos>
 				</HeadContainer>
 				<RowContainer id="RowContainer">
@@ -259,39 +231,8 @@ function User(props) {
 					{props.user.likes && <Box>{"Already likes you"}</Box>}
 					{props.user.isSeen && <Box>{"Already saw your profile"}</Box>}
 				</Bottom>
-				{/* <ButtonsContainer>
-					<div onClick={previousMatch}>
-						<i className="fas fa-chevron-left fa-3x"></i>
-					</div>
-					<div onClick={likeMatch}>
-						{like ? <Favorite fontSize="large"/> : <FavoriteBorder fontSize="large"/>}
-					</div>
-					<div onClick={nextMatch}>
-						<i className="fas fa-chevron-right fa-3x"></i>
-					</div>
-				</ButtonsContainer> */}
 			</Link>
 		</UserContainer>
-
-		// <UserContainer id={props.user.username}>
-		// 	<PaperContainer elevation={3} component="div">
-		// 		<Link to={{pathname: '/profile', state: { user: props.user }}} style={{ textDecoration: 'none' }}>
-		// 			<img src={props.user.pictures[0].url} alt={props.user.pictures[0].name} key={props.user.pictures[0].name} style={{width: '70vw'}}/>
-		// 			<Name>{props.user.firstname} {props.user.lastname}</Name>
-		// 			<InfoContainer>
-		// 				<Info>{props.user.gender.name.charAt(0).toUpperCase() + props.user.gender.name.slice(1)}</Info>
-		// 				<Info style={{marginLeft: '2vw'}}>{findAge(props.user.birthdate)} years old</Info>
-		// 				<Info style={{marginLeft: '2vw'}}>{props.user.orientation.name}</Info>
-		// 			</InfoContainer>
-		// 			<Hobbies>Interested in {userHobbies}</Hobbies>
-		// 			<Biography>{props.user.biography}</Biography>
-		// 		</Link>
-		// 		<ActionContainer>
-		// 			{ like === false ? <FavoriteIcon onClick={likeMatch} htmlColor='#FAE3D9' /> : <CancelIcon onClick={unlikeMatch} htmlColor='#FAE3D9' />}
-		// 			{ block === false ? <BlockIcon onClick={blockMatch} style={{marginLeft: "1.5em"}}></BlockIcon> : <ReplayIcon onClick={unblockMatch} style={{marginLeft: "1.5em"}}></ReplayIcon>}
-		// 		</ActionContainer>
-		// 	</PaperContainer>
-		// </UserContainer>
 	);
 }
 
